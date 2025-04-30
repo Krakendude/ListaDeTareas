f311fdf4dc3e481e51e245964ccf8db54e812c18
ramiro <radelacruzve@gmail.com>
2025-04-30 17:49:07 +0200

    sumethin

---
+++ app/src/main/java/com/example/listadetareas/Category.kt
@@ -0,0 +1,14 @@
+package com.example.listadetareas
+
+data class Category(
+    val id: Long,
+    val title: String
+)  {
+    companion object
+        {const val TABLE_NAME = "Categories"
+
+        const val COLUMN_NAME_ID = "id"
+        const val COLUMN_NAME_TITLE = "title"
+    }
+
+}
